5efb80c0f4d179d977232e1983c4626683072f5f
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2015-05-26 21:38:09 +0200

    moar json!

--- hello.py
+++ hello.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
         return "Hello World!"
+
+@app.route('/min.json')
+def mine():
+        with open('min.json', 'r') as ctx:
+                return ctx.read()
+        return '{"error":"Couldn\'t open file for reading"}'
+
